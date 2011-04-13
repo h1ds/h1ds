de449636be0bf8f3023fcfe39342fb00f9828b73
Dave Pretty <david.pretty@anu.edu.au>
2011-04-13 18:33:34 +1000

    include package data in setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from distutils.core import setup
 setup(name='h1ds_core',
       version='0.1',
       packages=['h1ds_core'],
+      package_data={'h1ds_core':['app_media/h1ds_core/css/*.css', 'templates/h1ds_core/*.html']}
       )
