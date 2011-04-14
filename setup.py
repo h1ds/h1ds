55f005387329fa35b5764f460a725e219b8f59d2
Dave Pretty <david.pretty@anu.edu.au>
2011-04-14 13:14:24 +1000

    add version number

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
+
+execfile('h1ds_core/version.py')
+
 setup(name='h1ds_core',
-      version='0.1',
+      version=__version__,
       packages=['h1ds_core'],
       package_data={'h1ds_core':['app_media/h1ds_core/css/*.css', 'templates/*/*.html']}
       )
