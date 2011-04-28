bd9276abfb2a3691815f5cd3a33d7235b9a80386
Dave Pretty <david.pretty@anu.edu.au>
2011-04-28 13:55:13 +1000

    fix templates

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ execfile('h1ds_summary/version.py')
 setup(name='h1ds_summary',
       version=__version__,
       packages=['h1ds_summary','h1ds_summary.migrations'],
+      package_data={'h1ds_summary':['templates/*/*.html',]}
       )
