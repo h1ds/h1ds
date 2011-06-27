decffad171a3d0f6aa50b9a7d18179f9bd9d447a
Dave Pretty <david.pretty@gmail.com>
2011-06-27 11:43:33 +1000

    add option to bootstrap to use development app list (checkout git repos with ssh so I can push back)

--- bootstrap.py
+++ bootstrap.py
@@ -3,7 +3,6 @@
 # Bootstrap and setup a virtualenv with the specified requirements.txt
 import os
 import sys
-import shutil
 import subprocess
 from optparse import OptionParser
 
@@ -12,6 +11,8 @@ usage = """usage: %prog [options]"""
 parser = OptionParser(usage=usage)
 parser.add_option("-c", "--clear", dest="clear", action="store_true",
                   help="clear out existing virtualenv")
+parser.add_option("-d", "--dev", dest="dev", action="store_true",
+                  help="Use requirements/apps.dev.txt")
 
 
 def main():
@@ -24,8 +25,12 @@ def main():
     if options.clear:
         subprocess.call(["virtualenv", "--clear", "--distribute", virtualenv])
     file_path = os.path.dirname(__file__)
-    subprocess.call(["pip", "install", "-E", virtualenv, "--requirement",
-                     os.path.join(file_path, "requirements/apps.txt")])
+    if options.dev:
+        subprocess.call(["pip", "install", "-E", virtualenv, "--requirement",
+                         os.path.join(file_path, "requirements/apps.dev.txt")])
+    else:
+        subprocess.call(["pip", "install", "-E", virtualenv, "--requirement",
+                         os.path.join(file_path, "requirements/apps.txt")])
 
 
 if __name__ == "__main__":
