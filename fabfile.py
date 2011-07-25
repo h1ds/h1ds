abc9645caebd6bcfa04dcae161d5bdad0350fe6d
Dave Pretty <david.pretty@anu.edu.au>
2011-07-25 14:24:23 +1000

    special treatment for moin - seems not to be pip friendly yet

--- fabfile.py
+++ fabfile.py
@@ -33,9 +33,21 @@ def setup():
     run('%(mkvirtualenv)s %(venv)s' % env)
     with prefix('workon %(venv)s' %env):
         run('cd $VIRTUAL_ENV && git clone %(git_url)s %(project)s' % env)
+        run('mkdir $VIRTUAL_ENV/src && git clone %(moin_git_url)s moinmoin' % env)
+        run('mkdir $VIRTUAL_ENV/wikidata')
 
-
+def setup_moin():
+    env.settings = '%(project)s.settings_%(environment)s' % env
+    env.venv = "%(project)s_%(environment)s" %env    
+    
+    with prefix('workon %(venv)s' %env):
+        env.virtual_env = os.environ['VIRTUAL_ENV']
+        with cd("%(virtual_env)s/src/moinmoin" %env):
+            run('git pull')
+            run('python setup.py install --force --install-data=%(virtual_env)s/wikidata --record=install.log' % env)
+    
 def deploy():
+    setup_moin()
     env.settings = '%(project)s.settings_%(environment)s' % env
     env.venv = "%(project)s_%(environment)s" %env    
     
