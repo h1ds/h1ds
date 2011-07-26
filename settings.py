705e039b54bcf4df9d0f5b2dbf07e0e4099ac4f0
Dave Pretty <david.pretty@anu.edu.au>
2011-07-27 09:35:31 +1000

    add 500 and 404 templates

--- settings.py
+++ settings.py
@@ -109,9 +109,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'h1ds.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(THIS_DIR, 'templates'),
 )
 
 INSTALLED_APPS = (
