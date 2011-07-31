8bf78776f612c8027134fca2295845cb52a63029
Dave Pretty <david.pretty@anu.edu.au>
2011-07-31 19:26:11 +1000

    add configdb urls

--- urls.py
+++ urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
                        (r'^openid/', include('django_openid_auth.urls')),
                        (r'^mdsplus/', include('h1ds_mdsplus.urls')),
                        (r'^summary/', include('h1ds_summary.urls')),
+                       (r'^configurations/', include('h1ds_configdb.urls')),
                        )
 
