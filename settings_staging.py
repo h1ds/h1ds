76282cc63087a48ab548d976d087b8dc72d0600a
Dave Pretty <david.pretty@anu.edu.au>
2011-08-16 12:08:05 +1000

    update staging settings

--- settings_staging.py
+++ settings_staging.py
@@ -5,7 +5,6 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # TODO: don't use absolute path name
-        'NAME': '/home/dave/.virtualenvs/h1ds_staging/db/h1ds_staging.db'
+        'NAME': 'h1ds_staging.db'
             }
     }
