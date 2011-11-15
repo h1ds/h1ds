fa7ff2d37d7aa59364c309e829876e29457628fd
David Pretty <david.pretty@anu.edu.au>
2011-11-15 15:58:31 +1100

    include mdsplus js in single js file for now

--- h1ds_core/static/js/script.js
+++ h1ds_core/static/js/script.js
@@ -68,65 +68,72 @@ function autoUpdateLatestShot() {
 }
 
 function formatDataForPlots(data) {
-       //console.log(data);
-       if (data.sigmin == undefined) {
-       var d = Array(data.node_data.length);
-       for( i=0; i < d.length; i++){
-         d[i]=[data.node_dim[i],data.node_data[i]];
-     }
-       var dataset = [{data:d, color:"rgb(50,50,255)"}];
-       return dataset;
-}
-       var d = {"signalmin":{"data": Array(data.sigmin.node_data.length)},
-                "signalmax":{"data": Array(data.sigmax.node_data.length)}};
-       for( i=0; i < d.signalmin.data.length; i++){
-         d.signalmin.data[i]=[data.sigmin.node_dim[i],data.sigmin.node_data[i]];
-         d.signalmax.data[i]=[data.sigmax.node_dim[i],data.sigmax.node_data[i]];
-     }
-     var dataset = [{id: 'sigmin', data:d.signalmin.data, lines:{show:true, lineWidth:0.3, fill:false, shadowSize:0}, color:"rgba(50,50,255,0.5)"},
-		     {id: 'sigmax', data:d.signalmax.data, lines:{show:true, lineWidth:0.3, fill:0.5, shadowSize:0}, color:"rgba(50,50,255,0.5)", fillBetween: 'sigmin'}];
-     return dataset;
+    // If we have min and max signals, then we fill the area between them.
+    if (($.inArray("min", data.labels) > -1) && ($.inArray("max", data.labels) > -1) && (data.labels.length == 2)) {
+	var d = {"signalmin":{"data": Array(data.data[0].length)},
+		 "signalmax":{"data": Array(data.data[1].length)}};
+
+	for( i=0; i < d.signalmin.data.length; i++){
+            d.signalmin.data[i]=[data.dim[i],data.data[0][i]];
+            d.signalmax.data[i]=[data.dim[i],data.data[1][i]];
+	}
+	var dataset = [{id: 'sigmin', data:d.signalmin.data, lines:{show:true, lineWidth:0.3, fill:false, shadowSize:0}, color:"rgba(50,50,255,0.5)"},
+		       {id: 'sigmax', data:d.signalmax.data, lines:{show:true, lineWidth:0.3, fill:0.5, shadowSize:0}, color:"rgba(50,50,255,0.5)", fillBetween: 'sigmin'}];
+	return dataset;	
+    }
+    
+    else {
+	var d = Array(data.data.length);
+	for( i=0; i < d.length; i++){
+            d[i]=[data.dim[i],data.data[i]];
+	}
+	var dataset = [{data:d, color:"rgb(50,50,255)"}];
+	return dataset;
+    }
 } // end formatDataForPlots
 
 function plotSignals() {
-  var original_query = " TODO: FIX {{ request_query|safe }}";
-  var new_query = $.extend({}, original_query, {'view':'json', 'f999_ResampleMinMax':600})
-  $.get(
-    '',new_query,
-     function (data) {
-       var dataset = formatDataForPlots(data);
-     var options = {selection:{mode:"x"}};
-     var sigplot = $.plot($("#signal-placeholder"),  dataset, options  );
-     var overviewplot = $.plot($("#signal-overview"),  dataset , options  );
-
-     $("#signal-placeholder").bind("plotselected", function (event, ranges) {
-        // do the zooming
-        var new_query = $.extend({}, original_query, {'view':'json', 'f980_DimRange':ranges.xaxis.from+'_'+ranges.xaxis.to, 'f990_ResampleMinMax':300});
-        //console.log(new_url);
-        $.get('', new_query, function(newdata) {
-        var new_d = formatDataForPlots(newdata);
-
-        sigplot = $.plot($("#signal-placeholder"), new_d, options);
-
-		     });
-		     
-
-
-
-
-        // don't fire event on the overview to prevent eternal loop
-        overviewplot.setSelection(ranges, true);
-    });
+    // TODO: there is a neater way (though perhaps not any quicker) to manipulate URL query strings...
+    if (window.location.search.length) {
+	var query_join_char = '&';
+    } else {
+	var query_join_char = '?';
+    }
+    var new_query = window.location.search + query_join_char + 'view=json&f999_name=resample_minmax&f999_arg1=600';
     
-    $("#signal-overview").bind("plotselected", function (event, ranges) {
-        sigplot.setSelection(ranges);
-    });
-
-
-     },
-     'json'
-  );
+    $.get(
+	new_query,
+	function (data) {
+	    var dataset = formatDataForPlots(data);
+	    var options = {selection:{mode:"x"}};
+	    var sigplot = $.plot($("#signal-placeholder"),  dataset, options  );
+	    var overviewplot = $.plot($("#signal-overview"),  dataset , options  );
+	    
+	    $("#signal-placeholder").bind("plotselected", function (event, ranges) {
+		// do the zooming
+		var new_query = window.location.search + query_join_char + 'view=json&f980_name=dim_range&f980_arg1='+ranges.xaxis.from+'&f980_arg2='+ranges.xaxis.to+'&f990_name=resample_minmax&f990_arg1=600';
+		$.get(new_query, function(newdata) {
+		    var new_d = formatDataForPlots(newdata);
+		    
+		    sigplot = $.plot($("#signal-placeholder"), new_d, options);
+		    
+		});
+		
+		// don't fire event on the overview to prevent eternal loop
+		overviewplot.setSelection(ranges, true);
+	    });
+	    
+	    $("#signal-overview").bind("plotselected", function (event, ranges) {
+		sigplot.setSelection(ranges);
+	    });
+	    
+	    
+	},
+	'json'
+    );
 }
+
+
 
 $('#full-content-container').masonry({
     itemSelector: '.mbox',
@@ -143,6 +150,10 @@ $(document).ready(function() {
     $(".mds-node-item").each(function() {
 	populateMDSNav(tree, shot, $(this));
     });
+    if ($("#signal-placeholder").length) {
+	data = plotSignals();
+    }
+
 });
 
 
