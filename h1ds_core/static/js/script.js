f12b938ff03016201a98d74eccc8355ff87768ef
David Pretty <david.pretty@anu.edu.au>
2011-11-17 14:10:15 +1100

    initial use of jQuery UI

--- h1ds_core/static/js/script.js
+++ h1ds_core/static/js/script.js
@@ -140,6 +140,38 @@ $('#masonry-container').masonry({
      columnWidth: 384
  });
 
+
+// start from http://jqueryui.com/demos/sortable/#portlets
+
+$(function() {
+    $( ".column" ).sortable({
+	connectWith: ".column"
+    });
+    $( ".column-thin" ).sortable({
+	connectWith: ".column-thin, .column-wide"
+    });
+    $( ".column-wide" ).sortable({
+	connectWith: ".column-thin, .column-wide"
+    });
+
+    $( ".portlet" ).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+	.find( ".portlet-header" )
+	.addClass( "ui-widget-header ui-corner-all" )
+	.prepend( "<span class='ui-icon ui-icon-minusthick'></span>")
+	.end()
+	.find( ".portlet-content" );
+
+    $( ".portlet-header .ui-icon" ).click(function() {
+	$( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
+	$( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
+    });
+
+    $( ".column" ).disableSelection();
+});
+
+// end from http://jqueryui.com/demos/sortable/#portlets
+
+
 $(document).ready(function() {
     updateLatestShot();
     autoUpdateLatestShot();
