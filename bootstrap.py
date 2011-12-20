c037a31211c2039ff680d6aea57f59de7fe71c14
David Pretty <david.pretty@anu.edu.au>
2011-12-20 15:40:40 +1100

    use single requirements file. require supervisor

--- bootstrap.py
+++ bootstrap.py
@@ -11,9 +11,6 @@ usage = """usage: %prog [options]"""
 parser = OptionParser(usage=usage)
 parser.add_option("-c", "--clear", dest="clear", action="store_true",
                   help="clear out existing virtualenv")
-parser.add_option("-d", "--dev", dest="dev", action="store_true",
-                  help="Use requirements/apps.dev.txt")
-
 
 def main():
     if "VIRTUAL_ENV" not in os.environ:
@@ -25,12 +22,8 @@ def main():
     if options.clear:
         subprocess.call(["virtualenv", "--clear", "--distribute", virtualenv])
     file_path = os.path.dirname(__file__)
-    if options.dev:
-        subprocess.call(["pip", "install", "-E", virtualenv, "--requirement",
-                         os.path.join(file_path, "requirements/apps.dev.txt")])
-    else:
-        subprocess.call(["pip", "install", "-E", virtualenv, "--requirement",
-                         os.path.join(file_path, "requirements/apps.txt")])
+    subprocess.call(["pip", "install", "-E", virtualenv, "--requirement",
+                     os.path.join(file_path, "requirements.txt")])
 
 
 if __name__ == "__main__":
