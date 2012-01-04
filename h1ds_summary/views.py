17455b10c57da191112016a66fcf0dafe88b4e1a
David Pretty <david.pretty@anu.edu.au>
2012-01-04 17:31:18 +1100

    remove unused view code and clean up import statements

--- h1ds_summary/views.py
+++ h1ds_summary/views.py
@@ -1,91 +1,25 @@
-
-from django.shortcuts import render_to_response
-from django.core.cache import cache
-from django.db.models import Max
-from django.views.decorators.cache import never_cache
-from django.utils import simplejson
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import RequestContext
-from django import forms
-from django.db import connection
-from django.core.urlresolvers import reverse
-from django.contrib import messages
-
-from h1ds_summary.models import SummaryAttribute
-
-DEFAULT_SHOT_REGEX = "last10"
-
-def do_html_data(data, attr_list):
-    output_data = []
-    for row in data:
-        row_data = [row[0]]
-        for attr_i,attr_data in enumerate(row[1:]):
-            attr_value = attr_data
-            if attr_list[attr_i].display_format:
-                try:
-                    attr_value = attr_list[attr_i].display_format %(attr_data)
-                except:
-                    pass
-            row_data.append({'val':attr_value, 'norm':int(100.0*attr_list[attr_i].normalise(attr_data)), 'col':attr_list[attr_i].colourise(attr_data), 'show_barplot':attr_list[attr_i].show_barplot()})
-        output_data.append(row_data)
-    return output_data
-
-
-def get_vis_attr_data(path, attr_list):
-    if len(attr_list) == 1:
-        return [{'att':attr_list[0], 'newpath':None}]
-    output = []
-    spl_path = path.split("/")
-    if len(spl_path) == 3: # path=/summary/ -> add default shots and another trailing /
-        spl_path[2] = DEFAULT_SHOT_REGEX
-        spl_path.append("")
-    if len(spl_path) == 4:
-        spl_path[3] = 'all'
-        spl_path.append("")
-    for att in attr_list:
-        newpath = '+'.join([i.slug for i in attr_list if not i==att])
-        spl_path[3] = newpath
-        output.append({'att':att, 'newpath':'/'.join(spl_path)})
-    return output
-
-
-def get_hidden_attr_data(path, attr_list, n_cols=2):
-    all_attrs = SummaryAttribute.objects.all()
-    output = []
-    spl_path=path.split('/')
-    if len(spl_path) == 3: # path=/summary/ -> add default shots and another trailing /
-        spl_path[2] = DEFAULT_SHOT_REGEX
-        spl_path.append("")
-    tmp_path = '+'.join([i.slug for i in attr_list])
-    for a in all_attrs:
-        if not a in attr_list:
-            spl_path[3] = "+".join([tmp_path, a.slug])
-            output.append({'att':a, 'newpath':'/'.join(spl_path)})
-    sorted_list = sorted(output, key=lambda k: k['att'].slug.lower())
-    sorted_output = []
-    n_rows = float(len(sorted_list))/n_cols
-    n_delta =int( n_rows)
-    if n_rows%1 != 0:
-        n_rows +=1
-    n_rows = int(n_rows)
-    for j in range(n_rows):
-        sorted_output.extend(sorted_list[j::n_rows])
-    return sorted_output
-
-########################################################################
-### NEW CODE                                                         ###
-########################################################################
-
 import json
 from urlparse import urlparse, urlunparse
 
-from django.core.urlresolvers import resolve
-from django.http import QueryDict
-from django.shortcuts import redirect
+from django import forms
+from django.contrib import messages
+from django.core.cache import cache
+from django.core.urlresolvers import resolve, reverse
+from django.db import connection
+from django.db.models import Max
+from django.http import QueryDict, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+from django.utils import simplejson
+from django.views.decorators.cache import never_cache
+from django.views.generic import View
+
 from h1ds_summary import SUMMARY_TABLE_NAME
 from h1ds_summary.forms import SummaryAttributeForm
+from h1ds_summary.models import SummaryAttribute
 from h1ds_summary.utils import parse_shot_str, parse_attr_str, parse_filter_str
-from django.views.generic import View
+
+DEFAULT_SHOT_REGEX = "last10"
 
 class SummaryView(View):
     
