18b995420acb2b68302efb44f562df7c79e87816
David Pretty <david.pretty@anu.edu.au>
2012-03-13 13:44:33 +1100

    refactor js: getPlotQueryString

--- h1ds_core/static/js/script.js
+++ h1ds_core/static/js/script.js
@@ -1,5 +1,5 @@
 ﻿// Custom javascript code for H1 data system
-// David Pretty, 2010
+// David Pretty, 2010-2012
 
 var isOdd = function(someNumber){
     return (someNumber%2 == 0) ? false : true;
@@ -68,7 +68,7 @@ function updateLatestShot() {
 function autoUpdateLatestShot() {
     setInterval(updateLatestShot, 2000);
 }
-
+/*
 function formatDataForPlots(data) {
     // If we have min and max signals, then we fill the area between them.
     if (($.inArray("min", data.labels) > -1) && ($.inArray("max", data.labels) > -1) && (data.labels.length == 2)) {
@@ -93,7 +93,7 @@ function formatDataForPlots(data) {
 	return dataset;
     }
 } // end formatDataForPlots
-
+*/
 
 $('#masonry-container').masonry({
      itemSelector: '.mbox',
@@ -239,6 +239,388 @@ function isMinMaxPlot(signal_data) {
     return  (($.inArray("min", signal_data.labels) > -1) && 
 	     ($.inArray("max", signal_data.labels) > -1) && 
 	     (signal_data.labels.length == 2)) ? true : false;
+}
+
+
+/* 
+ * Signal class constructor
+ *
+ * A signal is one or more 1-d arrays which share the same domain (e.g. timebase).
+ *
+ * arguments:
+ * data - the JSON object returned from H1DS server.
+ *
+ */
+
+function Signal(data) {
+
+}
+
+Signal.prototype = {
+    
+}
+
+/*
+ * getPlotQueryString
+ *
+ * Generate a URL query string for filtered data.
+ *
+ * arguments: any number of [filter_number, filter_name, arg1, arg2,...]
+ * returns: current URL with query string generated from filters.
+ *
+ * Example:
+ *     getPlotURL(['1','filter_a','10','20'], ['999','filter_b'])
+ * returns:
+ *   ?(current query string)&f1_name=filter_a&f1_arg0=10&f1_arg1=20&\
+ *   f999_name=filter_b&view=json
+ *
+ */
+function getPlotQueryString() {
+    // If the current URL has a query string, then we append to to ('&')
+    // otherwise we create it '?'.
+    var plot_url = window.location.search + (window.location.search.length ? '&' : '?');
+
+    for (var i=0; i < arguments.length; i++) {
+	if (i>0) plot_url += '&';
+	plot_url += 'f'+arguments[i][0]+'_name='+arguments[i][1];
+	for (var j=2; j< arguments[i].length; j++) {
+	    plot_url += '&f'+arguments[i][0]+'_arg'+(j-2)+'='+arguments[i][j];
+	}
+    }
+    
+    return (plot_url += '&view=json');
+}
+
+
+
+//
+// new_plotSignal1D
+//
+// For a given  container div, we fetch data subsampled  to the width of
+// the plot window.  The heights  of the plots are fixed.  Several plots
+// are actally displayed.  A large plot which allows  zooming (TODO: and
+// panning), and  a smaller (smaller  height) plot which shows  the full
+// signal and highlights the region shown in the larger plot.  An FFT of
+// the signal in the large plot is also shown.  TODO: Allow both panning
+// and  zooming  of  plot  TODO:  more  usable FFT  plots,  maybe  a  2D
+// spectrogram plot, which is selectable and applies filters to the freq
+// range selected.
+function new_plotSignal1D(id) {
+    // Get margins and plot width  for container, so we can resample the
+    // data to fit the window width.
+    var width_data = getPlotWidth(id);
+
+
+    // compile the URL for retrieving subsampled data
+    var trace_query = getPlotQueryString([999, 'resample_minmax', width_data.plot]);
+
+    
+    // spacing between bottom of plot and container.
+    // TODO: should merge this with getPlotWidth to have a single
+    // function which provides margins.
+    var bottom_spacing = 20;
+
+    // height of the signal plot 
+    var signal_height = 200, overview_height = 100;
+
+
+    // this is a bit convoluted, get the data, pass if to getFourier, which passes it to dataReady.
+    // getFourier doesn't actually use the data which it takes as an argument! just passes it along.
+    // TODO: refactor so it is more easily understandable.
+    $.getJSON(trace_query, getFourier);
+
+    function getFourier(d) {
+	// compile the URL to get power spectrum data.
+	var fourier_query = getPlotQueryString([990, 'power_spectrum'],[995, 'norm_dim_range',0.0, 0.5],[999, 'resample_minmax',width_data.plot]);
+	// get power spectrum data and pass the resampled data (which getFourier took as an argument) to dataReady.
+	$.getJSON(fourier_query, function(a) { dataReady(d,a) ;});
+    }
+
+    function dataReady(signal_data, fourier_data) {
+	var orig_data = signal_data;
+	// how far past the min and max data values should the axes plot?
+	// e.g. max y axis value = max_data + range_padding*(min_data - max_data)
+	var range_padding = 0.05;
+	// spacing between plots
+	var plot_spacing = 25;
+
+	// initially, the overview is the same as the signal data
+	var data = [{'name':'signal','data':signal_data, 'height':signal_height},
+		    {'name':'overview','data':orig_data, 'height':overview_height},
+		    {'name':'fourier','data':fourier_data, 'height':overview_height}]
+
+
+	// x and y are objects which will contain axes for each of the data plots/
+	var x = {}, y={};
+	// plot width
+	var w = width_data.plot;// + 2*width_data.margin;
+	//var m = width_data.margin;
+
+	// populate x and y with axes.
+	function setupAxes(d, i) {
+	    y[d.name] = d3.scale.linear();
+	    x[d.name] = d3.scale.linear();
+	}
+
+	// include additional info in data.
+	function updateData(d, i) {
+	    // offsets to determine vertical coordinates of each plot.
+	    if (i > 0) {
+		d.offset = d.height+data[i-1].offset+plot_spacing;
+	    } else {
+		d.offset = d.height;
+	    }
+	    // get min and max data values
+	    d.is_minmax = isMinMaxPlot(d.data);
+	    d.min_data = d.is_minmax ? d3.min(d.data.data[0]) : d3.min(d.data.data);
+	    d.max_data = d.is_minmax ? d3.max(d.data.data[1]) : d3.max(d.data.data);
+
+	    // get min and max values for axes
+	    d.delta_data = d.max_data - d.min_data;
+	    d.min_plot = d.min_data - range_padding*d.delta_data;
+	    d.max_plot = d.max_data + range_padding*d.delta_data;
+
+	    // get min and max dimension (i.e. x axis, e.g. time)
+	    d.min_dim = d.data.dim[0];
+	    d.max_dim = d.data.dim[d.data.dim.length-1];
+
+	    // recompute axes
+	    y[d.name].domain([d.min_data, d.max_data]).range([0 + range_padding*d.height, d.height*(1-range_padding)]);
+	    x[d.name].domain([d.min_dim, d.max_dim]).range([0 + width_data.marginLeft, w - width_data.marginRight]);	    
+	}
+
+	// apply the above axes and data functions
+	data.forEach(setupAxes);
+	data.forEach(updateData);
+
+	// fix height of container element
+	$(id).height(data[data.length -1].offset+bottom_spacing);
+
+	// add to the container the SVG element which will contain the plots.
+	var svg = d3.select(id)
+	    .append("svg:svg")
+	    .attr("width", w)
+	    .attr("height", data[data.length -1].offset+bottom_spacing);
+	
+	// add an SVG group ('g') container element for each plot.
+	var g = svg.selectAll("g")
+	    .data(data)
+	    .enter().append("svg:g")
+	    .attr("transform", function(d) {return "translate(0, "+d.offset+")" });
+
+	// for each data group, add an SVD path element with the data.
+	g.append("svg:path")
+	    .attr("class", "plot")
+	    .classed("path-filled", function(d) { return d.is_minmax })
+	    .attr("d", doLine);
+
+
+	// TODO: surely these can be replaced with d3 scales?
+	function minX(d) { return x[d.name](d.min_dim); }
+	function maxX(d) { return x[d.name](d.max_dim); }
+	function minY(d) { return -1 * y[d.name](d.min_plot); }
+	function maxY(d) { return -1 * y[d.name](d.max_plot); }
+	
+	// create a brush objects
+	var br = d3.svg.brush()
+	    .on("brushstart", brushstart)
+	    .on("brush", brush)
+	    .on("brushend", brushend);
+	
+	// this should be added by brush.js?
+	// probably we just need something else with .background
+	// and brush.js will insert?
+	// TODO: do we need this?
+	g.append("rect")
+            .attr("class", "background")
+            .style("visibility", "hidden")
+            .style("pointer-events", "all")
+            .style("cursor", "crosshair")
+	    .attr("x", minX)
+	    .attr("y", maxY)
+	    .attr("width", function(d) {return maxX(d)-minX(d)})
+	    .attr("height", function(d) {return minY(d)-maxY(d)}); 
+
+
+	// for each plot, call the brush and set the brush to the plot's x axis
+	g.each( function (d) { d3.select(this).call(br.x(x[d.name]))});
+
+	// fix the y range for the brush rectangles. 
+	// TODO: do we need this?
+	g.selectAll("rect.extent")
+	    .attr("y", maxY)
+	    .attr("height", function(d) {return minY(d)-maxY(d)});
+
+	//
+	// Brush functions
+	//
+
+	// called when brush starts
+	function brushstart(p) {
+	    g.classed("selecting", true);
+	    br.x(x[p.name]);
+	}
+
+
+	// called when brush is dragged
+	function brush() {
+	    var s = d3.event.target.extent();
+	}
+
+	// called when brush is finished (user unclicks the mouse)
+	function brushend() {
+	    g.classed("selecting", !d3.event.target.empty());
+	    var s = d3.event.target.extent();
+	    var new_data_url = getPlotQueryString([990, 'dim_range', s[0], s[1]],[999,'resample_minmax',width_data.plot]);
+
+	    function updateFourier(d) {
+		var fourier_query = getPlotQueryString([980, 'dim_range', s[0], s[1]], [990, 'power_spectrum'], [995, 'norm_dim_range', 0.0,0.5], [999, 'resample_minmax', width_data.plot]);
+		$.getJSON(fourier_query, function(a) { updatePlot(d,a) ;});
+	    }
+	    d3.json(new_data_url, updateFourier);
+
+	}
+
+	
+	function updatePlot(newdata, newfourier) {
+	    
+	    var ndat = [{'name':'signal','data':newdata, 'height':signal_height},
+			{'name':'overview', 'data':orig_data, 'height':overview_height},
+			{'name':'fourier','data':newfourier, 'height':overview_height}];
+
+	    ndat.forEach(updateData);
+
+	    g.data(ndat).select("path.plot")
+		.classed("path-filled", function(d, i) { return d.is_minmax })
+		.transition().duration(10).attr("d", doLine);
+	    
+	    g.selectAll(".xLabel").remove();
+	    g.selectAll(".xTicks").remove();
+	    g.selectAll(".yLabel").remove();
+	    g.selectAll(".yTicks").remove();
+
+	    updateAxesMarkers();
+
+
+	    g.each(function(d,i) {
+		    d3.select(this).call(br.clear());
+		if (i == 1 ) {
+		    d3.select(this).selectAll("rect.highlight").remove();
+		    d3.select(this).append("rect")
+			.attr("class", "highlight")
+			.attr("x", x['overview'](ndat[0].min_dim))
+			.attr("width", x['overview'](ndat[0].max_dim)-x['overview'](ndat[0].min_dim) )
+			.attr("y", -1*y['overview'](ndat[1].max_plot))
+			.attr("height", -1*(y['overview'](ndat[1].min_plot) - y['overview'](ndat[1].max_plot)));
+		    
+		}
+	    });
+
+	}
+
+
+
+
+	function doLine(d, i) {
+	    if (d.is_minmax) {
+		var fill_data = fillPlot(d.data);
+		var line = d3.svg.line()
+		    .x(function(a) {return x[d.name](a[0]); })
+		    .y(function(a) {return -1 * y[d.name](a[1]); });
+		return line(fill_data);
+	    } else {
+		var line = d3.svg.line()
+		    .x(function(a,j) { return x[d.name](d.data.dim[j]); })
+		    .y(function(a) { return -1 * y[d.name](a); });
+		return line(d.data.data);
+	    }
+	}
+	function xTicksWithData(d) {
+	    var t = [];
+	    var n = 5;
+	    var xticks = x[d.name].ticks(n);
+	    var xticklabels = x[d.name].tickFormat(n);
+	    for (var i=0; i < xticks.length; i++) t.push({'d':d, 't':xticks[i], 'lab':xticklabels});
+	    return t;
+	}
+	function yTicksWithData(d) {
+	    var t = [];
+	    var n = 5;
+	    var yticks = y[d.name].ticks(n);
+	    var yticklabels = y[d.name].tickFormat(n);
+	    for (var i=0; i < yticks.length; i++) t.push({'d':d, 't':yticks[i], 'lab':yticklabels});
+	    return t;
+	}
+
+
+	// x axis
+	g.append("svg:line")
+	    .attr("x1", minX)
+	    .attr("y1", minY)
+	    .attr("x2", maxX)
+	    .attr("y2", minY);
+	g.append("svg:line")
+	    .attr("x1", minX)
+	    .attr("y1", maxY)
+	    .attr("x2", maxX)
+	    .attr("y2", maxY);
+
+	// y axis
+	g.append("svg:line")
+	    .attr("x1", minX)
+	    .attr("y1", minY)
+	    .attr("x2", minX)
+	    .attr("y2", maxY);
+	g.append("svg:line")
+	    .attr("x1", maxX)
+	    .attr("y1", minY)
+	    .attr("x2", maxX)
+	    .attr("y2", maxY);
+
+	function updateAxesMarkers() {
+
+	g.selectAll(".xLabel")
+	    .data(xTicksWithData)
+	    .enter().append("svg:text")
+	    .attr("class", "xLabel")
+	    .text(function(a) { return a.lab(a.t) })
+	    .attr("x", function(a) { return x[a.d.name](a.t) })
+	    .attr("y", function(a) { return -1*(y[a.d.name](a.d.min_plot)-10) })
+	    .attr("text-anchor", "middle");
+
+	g.selectAll(".yLabel")
+	    .data(yTicksWithData)
+	    .enter().append("svg:text")
+	    .attr("class", "yLabel")
+	    .text(function(a) { return a.lab(a.t) })
+	    .attr("x", function(a) { return x[a.d.name](a.d.min_dim)-30 })
+	    .attr("y", function(a) { return -1*y[a.d.name](a.t) } )
+	    .attr("text-anchor", "right")
+	    .attr("dy", 4);
+
+	g.selectAll(".xTicks")
+	    .data(xTicksWithData)
+	    .enter().append("svg:line")
+	    .attr("class", "xTicks")
+	    .attr("x1", function(a) { return x[a.d.name](a.t); })
+	    .attr("y1", function(a) { return -1 * y[a.d.name](a.d.min_plot) })
+	    .attr("x2", function(a) { return x[a.d.name](a.t); })
+	    .attr("y2", function(a) { return -1 * (y[a.d.name](a.d.max_plot))});
+
+	g.selectAll(".yTicks")
+	    .data(yTicksWithData)
+	    .enter().append("svg:line")
+	    .attr("class", "yTicks")
+	    .attr("y1", function(a) { return -1 * y[a.d.name](a.t); })
+	    .attr("x1", function(a) { return x[a.d.name](a.d.min_dim) })
+	    .attr("y2", function(a) { return -1 * y[a.d.name](a.t); })
+	    .attr("x2", function(a) { return x[a.d.name](a.d.max_dim) });
+
+	}
+
+	updateAxesMarkers();
+
+    }    
 }
 
 
@@ -583,7 +965,7 @@ $(document).ready(function() {
 	populateMDSNav(tree, shot, $(this));
     });
     if ($("#signal-1d-placeholder").length) {
-	data = plotSignal1D("#signal-1d-placeholder");
+	data = new_plotSignal1D("#signal-1d-placeholder");
     }
     if ($("#signal-2d-placeholder").length) {
 	plotSignal2D();
