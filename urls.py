e60e1a378e3b89ba23eb82ec93c9f0df3b0ca978
David Pretty <david.pretty@anu.edu.au>
2012-05-18 22:41:41 +1000

    set MEDIA_ROOT and MEDIA_URL

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import direct_to_template
 from django.contrib import admin
-
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -16,3 +16,10 @@ urlpatterns = patterns('',
                        (r'^configurations/', include('h1ds_configdb.urls')),
                        )
 
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                'document_root': settings.MEDIA_ROOT,
+                }
+            ),
+    )
