34e4d8ab99581e5d81ce9a4a410f669de49809fc
David Pretty <david.pretty@anu.edu.au>
2013-03-01 16:29:03 +1100

    specify module root url

--- h1ds_summary/__init__.py
+++ h1ds_summary/__init__.py
@@ -1,7 +1,15 @@
 """Summary database for reduced MDSplus data."""
 
+from django.conf import settings
+
 # Name to be used for this module in H1DS header, footer links, etc.
 MODULE_DOC_NAME = "Summary"
+
+if hasattr(settings, "H1DS_SUMMARY_ROOT_URL"):
+    MODULE_ROOT_URL = settings.H1DS_SUMMARY_ROOT_URL
+else:
+    MODULE_ROOT_URL = "summary" 
+
 
 # Name of the SQLite summary table.
 SUMMARY_TABLE_NAME = "summary"
