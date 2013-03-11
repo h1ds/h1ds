c5f9a7879d5b0bbccda67a8cc3c80733b9f5d5a7
David Pretty <david.pretty@anu.edu.au>
2013-03-12 08:01:58 +1100

    provide value arg for h1ds signals

--- h1ds_core/signals.py
+++ h1ds_core/signals.py
@@ -2,7 +2,7 @@ import django.dispatch
 
 from h1ds_core.models import H1DSSignalInstance
 
-h1ds_signal = django.dispatch.Signal(providing_args=["h1ds_sig"])
+h1ds_signal = django.dispatch.Signal(providing_args=["h1ds_sig", "value"])
 
 def create_instance(sender, **kwargs):
     new_instance = H1DSSignalInstance(signal=kwargs['h1ds_sig'])
