1ded0cc8b6a74997f88a579f178e3d47653c3f99
David Pretty <david.pretty@anu.edu.au>
2013-03-12 14:28:44 +1100

    store signal value in saved instane

--- h1ds_core/signals.py
+++ h1ds_core/signals.py
@@ -5,7 +5,8 @@ from h1ds_core.models import H1DSSignal,H1DSSignalInstance
 h1ds_signal = django.dispatch.Signal(providing_args=["h1ds_sig", "value"])
 
 def create_instance(sender, **kwargs):
-    new_instance = H1DSSignalInstance(signal=kwargs['h1ds_sig'])
+    new_instance = H1DSSignalInstance(signal=kwargs['h1ds_sig'],
+                                      value=kwargs.get('value', ""))
     new_instance.save()
 
 h1ds_signal.connect(create_instance)
