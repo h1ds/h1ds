590caf397eade82dff027ff53f43c2e3a041b7d9
David Pretty <david.pretty@anu.edu.au>
2013-03-12 13:39:09 +1100

    add NewShotEvent to h1ds-core

--- h1ds_core/signals.py
+++ h1ds_core/signals.py
@@ -1,6 +1,6 @@
 import django.dispatch
 
-from h1ds_core.models import H1DSSignalInstance
+from h1ds_core.models import H1DSSignal,H1DSSignalInstance
 
 h1ds_signal = django.dispatch.Signal(providing_args=["h1ds_sig", "value"])
 
@@ -9,3 +9,14 @@ def create_instance(sender, **kwargs):
     new_instance.save()
 
 h1ds_signal.connect(create_instance)
+
+new_shot_inst, c = H1DSSignal.objects.get_or_create(name="new_shot",
+                                                    description="New Shot")
+
+class NewShotEvent(object):
+    def __init__(self, shot_number):
+        self.shot_number = shot_number
+    def send_event(self):
+        h1ds_signal.send(sender=self,
+                         h1ds_sig=new_shot_inst,
+                         value=self.shot_number)
