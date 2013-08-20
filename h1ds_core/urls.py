723d6d19f2a149353be13b4d2f538e164bb0e6ce
David Pretty <david.pretty@anu.edu.au>
2013-08-20 16:36:57 +1000

    fix typo in urls.py

--- h1ds_core/urls.py
+++ h1ds_core/urls.py
@@ -42,7 +42,7 @@ filter_patterns = patterns('',
     )
 
 usersignal_patterns = patterns('',
-    url(r'^add$/', UserSignalCreateView.as_view(), name="h1ds-add-user-signal"),
+    url(r'^add/$', UserSignalCreateView.as_view(), name="h1ds-add-user-signal"),
     url(r'^delete/(?P<pk>\d+)$', UserSignalDeleteView.as_view(), name="h1ds-delete-user-signal"),
     url(r'^update/(?P<pk>\d+)$', UserSignalUpdateView.as_view(), name="h1ds-update-user-signal"),
     )
