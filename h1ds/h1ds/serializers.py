76653cd6d44b51f11990ce441ecf2ae1d3319c3e
David Pretty <david.pretty@anu.edu.au>
2013-09-13 13:19:10 +1000

    remove unused code

--- h1ds/h1ds/serializers.py
+++ h1ds/h1ds/serializers.py
@@ -157,23 +157,6 @@ class NodeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Node
         fields = ('path', 'parent', 'children', 'data', 'url')
-
-    #data = serializers.SerializerMethodField('get_node_data')
-    
-    def get_node_data(self, obj):
-        d = obj.read_primary_data()
-        if np.isscalar(d) or d == None:
-            return d
-        else:
-            return d.tolist()
-        
-    """
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
-    slug = models.SlugField()
-    has_data = models.BooleanField(default=True)
-    dimension = models.PositiveSmallIntegerField(blank=True, null=True)
-    dtype = models.CharField(max_length=16)
-    """
     
 
 class FilterSerializer(serializers.Serializer):
