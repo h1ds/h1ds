c4da82f75b18fdf4b2749a30c2e0b9afd9dc2e56
David Pretty <david.pretty@anu.edu.au>
2013-10-16 14:51:21 +1100

    add --device option to addshot management command

--- h1ds/h1ds/management/commands/addshot.py
+++ h1ds/h1ds/management/commands/addshot.py
@@ -1,14 +1,25 @@
 """Add specified shots."""
+from optparse import make_option
 from django.core.management.base import BaseCommand
-from h1ds.models import Shot
+from h1ds.models import Shot, Device
 
 
 class Command(BaseCommand):
     args = '<shot_number shot_number ...>'
     help = 'Add specified shots.'
+    option_list = BaseCommand.option_list + (
+        make_option('-d', '--device',
+                    dest='device',
+                    help='Device to add shot to (use slug string)'
+                    ),
+    )
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
+        if options['device']:
+            device = Device.objects.get(slug=options['device'])
+        else:
+            device = Device.objects.get(is_default=True)
         for shot_number in map(int, args):
-            s = Shot(number=shot_number)
+            s = Shot(number=shot_number, device=device)
             s.save()
             self.stdout.write('Successfully added shot %d' % shot_number)
