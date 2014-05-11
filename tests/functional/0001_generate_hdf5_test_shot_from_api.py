9e88f9deacea2118b0ba0dfe2f6e2be4fdfbd9fb
David Pretty <david.pretty@stellulata.com>
2014-05-11 17:50:25 +1000

    functional test 0001 - use web api to add test shot

--- tests/functional/0001_generate_hdf5_test_shot_from_api.py
+++ tests/functional/0001_generate_hdf5_test_shot_from_api.py
@@ -26,19 +26,16 @@ class GenerateHdf5TestShotTest(TestCase):
         shot_numbers = xrange(1,10)
         device = Device.objects.create(name='test_hdf5_device',
                                 description='Test HDF5 Device',
-                                data_backend='hdf5')
-        
-        generate_test_data(device, shot_numbers)
+                                data_backend='hdf5',
+                                read_only=False)
 
-        for shot_number in shot_numbers:
-            shot = Shot.objects.get(device=device, number=shot_number)
-        
-        # check URLs to make sure we can see the data
-        #response = self.client.get('/data/test_hdf5_device/1/')
-        #assert 'operations' in response
-        #assert 'diagnostics' in response
-        
+        # use http post/put to create a new shot
 
+        # use http post/put to create branches, data etc
+
+        # make sure the new data appears in html
+
+                
 
         
 
