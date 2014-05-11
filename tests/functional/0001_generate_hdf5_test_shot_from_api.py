07635b53605008f4579febc3e189a9c854bc287d
David Pretty <david.pretty@stellulata.com>
2014-05-11 22:28:16 +1000

    functional test refactor

--- tests/functional/0001_generate_hdf5_test_shot_from_api.py
+++ tests/functional/0001_generate_hdf5_test_shot_from_api.py
@@ -16,30 +16,32 @@ class GenerateHdf5TestShotTest(TestCase):
 
     def setUp(self):
         #self.browser = webdriver.Firefox()
-        pass
+        self.device = Device.objects.create(name='test_hdf5_device',
+                                    description='Test HDF5 Device',
+                                    data_backend='hdf5',
+                                    read_only=False)
 
     def tearDown(self):
         #self.browser.quit()
         pass
 
     def test_generate_hdf5_test_shot_via_api(self):
-        shot_numbers = xrange(1,10)
-        device = Device.objects.create(name='test_hdf5_device',
-                                description='Test HDF5 Device',
-                                data_backend='hdf5',
-                                read_only=False)
 
         # use http post/put to create a new shot
         response = self.client.put('/data/test_hdf5_device/1/')
         self.assertEqual(response.status_code, 200)
 
-        new_shot = Shot.objects.get(device=device, number=1)
-        
+        new_shot = Shot.objects.get(device=self.device, number=1)
+
         # use http post/put to create branches, data etc
 
         # make sure the new data appears in html
 
-                
+    def test_generate_tree(self):
+        pass
+
+    def test_generate_pathnode(self):
+        pass
 
         
 
