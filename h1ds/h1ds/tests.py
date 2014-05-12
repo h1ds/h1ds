d8f218536841e90fa2ab0419e5abe1212624ae08
David Pretty <david.pretty@stellulata.com>
2014-05-12 12:29:54 +1000

    refactor unit tests

--- h1ds/h1ds/tests.py
+++ h1ds/h1ds/tests.py
@@ -1,6 +1,24 @@
 from django.test import TestCase
 from h1ds.models import Device, Shot
 
+class WritableDeviceTestCase(TestCase):
+    def setUp(self):
+        self.device_names = {'read_only':'readonly_hdf5_device',
+                             'read_write':'readwrite_hdf5_device'}
+        self.readonly_device = Device.objects.create(name=self.device_names['read_only'],
+                                description='Test HDF5 Device (read only)',
+                                data_backend='hdf5',
+                                read_only=True)
+        self.readonly_device.full_clean()
+        
+        self.readwrite_device = Device.objects.create(name=self.device_names['read_write'],
+                                description='Test HDF5 Device (read/write)',
+                                data_backend='hdf5',
+                                read_only=False)
+        self.readwrite_device.full_clean()
+
+
+
 class DeviceBackendTestCase(TestCase):
 
     def test_mds_backend(self):
@@ -23,37 +41,36 @@ class DeviceBackendTestCase(TestCase):
                                 data_backend='hdf5')
         device.full_clean()
 
-
-
-
-class ReadWriteDeviceTest(TestCase):
+class WebAPIPutShotTest(WritableDeviceTestCase):
 
     def test_read_write_device(self):
+        shot_number = 1
+        response = self.client.put('/data/{}/{}/'.format(self.device_names['read_write'], shot_number))
+        self.assertEqual(response.status_code, 200)
+        
+        new_shot = Shot.objects.get(device=self.readwrite_device, number=shot_number)
+
+    def test_read_only_device(self):
+        shot_number = 1
+        response = self.client.put('/data/{}/{}/'.format(self.device_names['read_only'], shot_number))
+        self.assertEqual(response.status_code, 405)
+        
+        #shot should not appear
+        with self.assertRaises(Shot.DoesNotExist):
+            Shot.objects.get(device=self.readonly_device, number=shot_number)
+        
+
+"""
+class WebApiPutTreeTest(TestCase):
+
+    
+    def test_put_tree(self):
         device = Device.objects.create(name='test_hdf5_device',
                                 description='Test HDF5 Device',
                                 data_backend='hdf5',
                                 read_only=False)
         device.full_clean()
 
-        response = self.client.put('/data/test_hdf5_device/1/')
+        response = self.client.put('/data/test_hdf5_device/1/diagnostics/')
         self.assertEqual(response.status_code, 200)
-        
-        new_shot = Shot.objects.get(device=device, number=1)
-        
-        
-        
-class ReadOnlyDeviceTest(TestCase):
-    
-    def test_read_write_device(self):
-        # default should be read only
-        device = Device.objects.create(name='test_hdf5_device',
-                                description='Test HDF5 Device',
-                                data_backend='hdf5')
-        device.full_clean()
-
-        response = self.client.put('/data/test_hdf5_device/1/')
-        self.assertEqual(response.status_code, 405)
-
-        #shot should not appear
-        with self.assertRaises(Shot.DoesNotExist):
-            Shot.objects.get(device=device, number=1)
+"""
